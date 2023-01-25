129b64447499a3a4558669dc37f20142ded0b6a8
Omar Cherni <89669057+ocherni@users.noreply.github.com>
2023-01-25 13:52:09 +0100

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,45 +1,70 @@
 #!/usr/bin/python3
-"""Script to get stats from a request"""
-
+"""prints logs"""
 import sys
 
-codes = {}
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-count = 0
-size = 0
 
-try:
-    for ln in sys.stdin:
-        if count == 10:
-            print("File size: {}".format(size))
-            for key in sorted(codes):
-                print("{}: {}".format(key, codes[key]))
-            count = 1
-        else:
-            count += 1
+def imp(fs, e0, e1, e2, e3, e4, e5, e6, e7):
+    """Method to print"""
+    print("File size: {}".format(fs))
+    if (e0 > 0):
+        print("200: {}".format(e0))
+    if (e1 > 0):
+        print("301: {}".format(e1))
+    if (e2 > 0):
+        print("400: {}".format(e2))
+    if (e3 > 0):
+        print("401: {}".format(e3))
+    if (e4 > 0):
+        print("403: {}".format(e4))
+    if (e5 > 0):
+        print("404: {}".format(e5))
+    if (e6 > 0):
+        print("405: {}".format(e6))
+    if (e7 > 0):
+        print("500: {}".format(e7))
 
-        ln = ln.split()
+if __name__ == "__main__":
+    """MAIN"""
+    c = 0
+    e0 = 0
+    e1 = 0
+    e2 = 0
+    e3 = 0
+    e4 = 0
+    e5 = 0
+    e6 = 0
+    e7 = 0
+    fs = 0
+    line = sys.stdin.readline()
+    try:
+        while line:
+            s = line.split()
+            if len(s) > 6:
+                if c == 10:
+                    imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
+                    c = 0
 
-        try:
-            size = size + int(ln[-1])
-        except (IndexError, ValueError):
-            pass
-
-        try:
-            if ln[-2] in status_codes:
-                if codes.get(ln[-2], -1) == -1:
-                    codes[ln[-2]] = 1
-                else:
-                    codes[ln[-2]] += 1
-        except IndexError:
-            pass
-
-    print("File size: {}".format(size))
-    for key in sorted(codes):
-        print("{}: {}".format(key, codes[key]))
-
-except KeyboardInterrupt:
-    print("File size: {}".format(size))
-    for key in sorted(codes):
-        print("{}: {}".format(key, codes[key]))
-    raise
+                n = s[-2]
+                if n == "200":
+                    e0 = e0 + 1
+                elif n == "301":
+                    e1 = e1 + 1
+                elif n == "400":
+                    e2 = e2 + 1
+                elif n == "401":
+                    e3 = e3 + 1
+                elif n == "403":
+                    e4 = e4 + 1
+                elif n == "404":
+                    e5 = e5 + 1
+                elif n == "405":
+                    e6 = e6 + 1
+                elif n == "500":
+                    e7 = e7 + 1
+                fs = fs + int(s[-1])
+                line = sys.stdin.readline()
+                c = c + 1
+    except KeyboardInterrupt:
+        imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
+        raise
+    imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
