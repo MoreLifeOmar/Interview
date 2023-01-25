80b478176401670308052454d5eda5657b76dc1c
Omar Cherni <89669057+ocherni@users.noreply.github.com>
2023-01-25 13:54:58 +0100

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,70 +1,47 @@
 #!/usr/bin/python3
-"""prints logs"""
-import sys
+""" script that reads stdin line by line and computes metrics """
 
+if __name__ == '__main__':
 
-def imp(fs, e0, e1, e2, e3, e4, e5, e6, e7):
-    """Method to print"""
-    print("File size: {}".format(fs))
-    if (e0 > 0):
-        print("200: {}".format(e0))
-    if (e1 > 0):
-        print("301: {}".format(e1))
-    if (e2 > 0):
-        print("400: {}".format(e2))
-    if (e3 > 0):
-        print("401: {}".format(e3))
-    if (e4 > 0):
-        print("403: {}".format(e4))
-    if (e5 > 0):
-        print("404: {}".format(e5))
-    if (e6 > 0):
-        print("405: {}".format(e6))
-    if (e7 > 0):
-        print("500: {}".format(e7))
+    import sys
 
-if __name__ == "__main__":
-    """MAIN"""
-    c = 0
-    e0 = 0
-    e1 = 0
-    e2 = 0
-    e3 = 0
-    e4 = 0
-    e5 = 0
-    e6 = 0
-    e7 = 0
-    fs = 0
-    line = sys.stdin.readline()
+    def print_results(statusCodes, fileSize):
+        """ Print statistics """
+        print("File size: {:d}".format(fileSize))
+        for statusCode, times in sorted(statusCodes.items()):
+            if times:
+                print("{:s}: {:d}".format(statusCode, times))
+
+    statusCodes = {"200": 0,
+                   "301": 0,
+                   "400": 0,
+                   "401": 0,
+                   "403": 0,
+                   "404": 0,
+                   "405": 0,
+                   "500": 0
+                   }
+    fileSize = 0
+    n_lines = 0
+
     try:
-        while line:
-            s = line.split()
-            if len(s) > 6:
-                if c == 10:
-                    imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
-                    c = 0
-
-                n = s[-2]
-                if n == "200":
-                    e0 = e0 + 1
-                elif n == "301":
-                    e1 = e1 + 1
-                elif n == "400":
-                    e2 = e2 + 1
-                elif n == "401":
-                    e3 = e3 + 1
-                elif n == "403":
-                    e4 = e4 + 1
-                elif n == "404":
-                    e5 = e5 + 1
-                elif n == "405":
-                    e6 = e6 + 1
-                elif n == "500":
-                    e7 = e7 + 1
-                fs = fs + int(s[-1])
-                line = sys.stdin.readline()
-                c = c + 1
+        """ Read stdin line by line """
+        for line in sys.stdin:
+            if n_lines != 0 and n_lines % 10 == 0:
+                """ After every 10 lines, print from the beginning """
+                print_results(statusCodes, fileSize)
+            n_lines += 1
+            data = line.split()
+            try:
+                """ Compute metrics """
+                statusCode = data[-2]
+                if statusCode in statusCodes:
+                    statusCodes[statusCode] += 1
+                fileSize += int(data[-1])
+            except Exception:
+                pass
+        print_results(statusCodes, fileSize)
     except KeyboardInterrupt:
-        imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
+        """ Keyboard interruption, print from the beginning """
+        print_results(statusCodes, fileSize)
         raise
-    imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
