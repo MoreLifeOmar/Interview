1d0d0810ba26573d3188e3c2839dff75fcaf24c8
Omar Cherni <89669057+ocherni@users.noreply.github.com>
2023-01-26 23:49:20 +0100

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,70 +1,59 @@
 #!/usr/bin/python3
-"""prints logs"""
+"""
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
+"""
 import sys
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-def imp(fs, e0, e1, e2, e3, e4, e5, e6, e7):
-    """Method to print"""
-    print("File size: {}".format(fs))
-    if (e0 > 0):
-        print("200: {}".format(e0))
-    if (e1 > 0):
-        print("301: {}".format(e1))
-    if (e2 > 0):
-        print("400: {}".format(e2))
-    if (e3 > 0):
-        print("401: {}".format(e3))
-    if (e4 > 0):
-        print("403: {}".format(e4))
-    if (e5 > 0):
-        print("404: {}".format(e5))
-    if (e6 > 0):
-        print("405: {}".format(e6))
-    if (e7 > 0):
-        print("500: {}".format(e7))
+def prn_stats():
+    """
+    Function that print stats about log
+    """
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
+
 
 if __name__ == "__main__":
-    """MAIN"""
-    c = 0
-    e0 = 0
-    e1 = 0
-    e2 = 0
-    e3 = 0
-    e4 = 0
-    e5 = 0
-    e6 = 0
-    e7 = 0
-    fs = 0
-    line = sys.stdin.readline()
+    cnt = 0
     try:
-        while line:
-            s = line.split()
-            if len(s) > 6:
-                if c == 10:
-                    imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
-                    c = 0
-
-                n = s[-2]
-                if n == "200":
-                    e0 = e0 + 1
-                elif n == "301":
-                    e1 = e1 + 1
-                elif n == "400":
-                    e2 = e2 + 1
-                elif n == "401":
-                    e3 = e3 + 1
-                elif n == "403":
-                    e4 = e4 + 1
-                elif n == "404":
-                    e5 = e5 + 1
-                elif n == "405":
-                    e6 = e6 + 1
-                elif n == "500":
-                    e7 = e7 + 1
-                fs = fs + int(s[-1])
-                line = sys.stdin.readline()
-                c = c + 1
+        """ Iter the standar input """
+        for data in sys.stdin:
+            try:
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
+            except:
+                pass
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
     except KeyboardInterrupt:
-        imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
+        prn_stats()
         raise
-    imp(fs, e0, e1, e2, e3, e4, e5, e6, e7)
+    else:
+        prn_stats()
