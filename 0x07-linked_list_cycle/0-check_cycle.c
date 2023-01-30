0bfe2a5d6cc4833662b7ba1434e37b728a8e6060
Omar Cherni <89669057+ocherni@users.noreply.github.com>
2023-01-30 20:09:15 +0100

    Create 0-check_cycle.c

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if list has a cycle
+ * @list: pointer to the list
+ * Return: 0 if is no cycle,1 if is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *lento = list, *rap = list;
+
+    if (list == NULL)
+        return (0);
+    while (lento && rap && rap->next)
+    {
+        lento = lento->next;
+        rap = rap->next->next;
+
+        if (lento == rap)
+        {
+            return (1);
+        }
+    }
+    return (0);
+}
